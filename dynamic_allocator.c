0a3373127bb3228e1a5cac430eb398049ef921e6
Fady <85071832+Fady278@users.noreply.github.com>
2025-12-06 20:14:01 +0200

    Minor bugs.  && edit in faulthandler
    in faulthandler removed the if !USE_KHEAP from lru
    fixed the bug in the bonus function that fady rafat wrote

---
+++ dynamic_allocator.c
@@ -0,0 +1,290 @@
+/*
+ * dynamic_allocator.c
+ *
+ *  Created on: Sep 21, 2023
+ *      Author: HP
+ */
+#include <inc/assert.h>
+#include <inc/string.h>
+#include "../inc/dynamic_allocator.h"
+
+//==================================================================================//
+//============================== GIVEN FUNCTIONS ===================================//
+//==================================================================================//
+//==================================
+//==================================
+// [1] GET PAGE VA:
+//==================================
+__inline__ uint32 to_page_va(struct PageInfoElement *ptrPageInfo)
+{
+	if (ptrPageInfo < &pageBlockInfoArr[0] || ptrPageInfo >= &pageBlockInfoArr[DYN_ALLOC_MAX_SIZE/PAGE_SIZE])
+			panic("to_page_va called with invalid pageInfoPtr");
+	// Get start VA of the page from the corresponding Page Info pointer
+	int idxInPageInfoArr = (ptrPageInfo - pageBlockInfoArr);
+	return dynAllocStart + (idxInPageInfoArr << PGSHIFT);
+}
+
+//==================================
+// [2] GET PAGE INFO OF PAGE VA:
+//==================================
+__inline__ struct PageInfoElement * to_page_info(uint32 va)
+{
+	int idxInPageInfoArr = (va - dynAllocStart) >> PGSHIFT;
+	if (idxInPageInfoArr < 0 || idxInPageInfoArr >= DYN_ALLOC_MAX_SIZE/PAGE_SIZE)
+		panic("to_page_info called with invalid pa");
+	return &pageBlockInfoArr[idxInPageInfoArr];
+}
+
+
+//==================================================================================//
+//============================ REQUIRED FUNCTIONS ==================================//
+//==================================================================================//
+
+//==================================
+// [1] INITIALIZE DYNAMIC ALLOCATOR:
+//==================================
+bool is_initialized = 0;
+void initialize_dynamic_allocator(uint32 daStart, uint32 daEnd)
+{
+	//==================================================================================
+	//DON'T CHANGE THESE LINES==========================================================
+	//==================================================================================
+	{
+		assert(daEnd <= daStart + DYN_ALLOC_MAX_SIZE);
+		is_initialized = 1;
+	}
+	//==================================================================================
+	//==================================================================================
+	//TODO: [PROJECT'25.GM#1] DYNAMIC ALLOCATOR - #1 initialize_dynamic_allocator
+	//Your code is here
+	//Comment the following line
+	//	panic("initialize_dynamic_allocator() Not implemented yet");
+
+	dynAllocStart = daStart;
+	dynAllocEnd = daEnd;
+
+	LIST_INIT(&freePagesList);
+
+	for (uint32 current_va = daStart; current_va < daEnd; current_va += PAGE_SIZE) {
+		 struct PageInfoElement *page = to_page_info(current_va);
+		 page->block_size = 0;
+		 page->num_of_free_blocks = 0;
+
+		 LIST_INSERT_TAIL(&freePagesList, page);
+	}
+
+	for (uint16 i = 0; i < NUM_OF_BLOCK_SIZES; ++i) {
+		LIST_INIT(&freeBlockLists[i]);
+	}
+
+}
+
+//===========================
+// [2] GET BLOCK SIZE:
+//===========================
+__inline__ uint32 get_block_size(void *va)
+{
+	//TODO: [PROJECT'25.GM#1] DYNAMIC ALLOCATOR - #2 get_block_size
+	//Your code is here
+	//Comment the following line
+	//	panic("get_block_size() Not implemented yet");
+
+	return to_page_info((uint32)va)->block_size;
+}
+
+//===========================
+// 3) ALLOCATE BLOCK:
+//===========================
+void *alloc_block(uint32 size)
+{
+	//==================================================================================
+	//DON'T CHANGE THESE LINES==========================================================
+	//==================================================================================
+	{
+		assert(size <= DYN_ALLOC_MAX_BLOCK_SIZE);
+	}
+	//==================================================================================
+	//==================================================================================
+	//TODO: [PROJECT'25.GM#1] DYNAMIC ALLOCATOR - #3 alloc_block
+	//Your code is here
+	//Comment the following line
+	//	panic("alloc_block() Not implemented yet");
+
+	if (size == 0) {
+		return NULL;
+	}
+
+	uint16 block_size = DYN_ALLOC_MIN_BLOCK_SIZE, idx = 0;
+	while (block_size < size) {
+		block_size <<= 1;
+		++idx;
+	}
+
+	// Found Free Block
+	if (LIST_SIZE(&freeBlockLists[idx]) > 0) {
+		struct BlockElement *block = LIST_FIRST(&freeBlockLists[idx]);
+
+		struct PageInfoElement *page = to_page_info((uint32)block);
+		page->num_of_free_blocks--;
+
+		LIST_REMOVE(&freeBlockLists[idx], block);
+		return (void *)block;
+	}
+
+	// Found Free Page
+	if (LIST_SIZE(&freePagesList) > 0) {
+		struct PageInfoElement *page = LIST_FIRST(&freePagesList);
+
+		uint32 current_va = to_page_va(page);
+		get_page((void *)current_va); // Allocate The Page In Physical Memory
+
+		uint16 num_of_free = PAGE_SIZE / block_size;
+
+		page->block_size = block_size;
+		page->num_of_free_blocks = num_of_free;
+
+		for (uint16 i = 0; i < num_of_free; ++i) {
+			struct BlockElement *block = (struct BlockElement *)current_va;
+			LIST_INSERT_TAIL(&freeBlockLists[idx], block);
+
+			current_va += block_size;
+		}
+
+		LIST_REMOVE(&freePagesList, page);
+	}
+
+	// Try to match any size >= block_size
+	while (idx < NUM_OF_BLOCK_SIZES) {
+
+		// Found Free Block
+		if (LIST_SIZE(&freeBlockLists[idx]) > 0) {
+			struct BlockElement *block = LIST_FIRST(&freeBlockLists[idx]);
+
+			struct PageInfoElement *page = to_page_info((uint32)block);
+			page->num_of_free_blocks--;
+
+			LIST_REMOVE(&freeBlockLists[idx], block);
+			return (void *)block;
+		}
+
+		++idx;
+	}
+
+	// No Free Blocks
+	panic("alloc_block Cannot Find Free Blocks");
+
+	//TODO: [PROJECT'25.BONUS#1] DYNAMIC ALLOCATOR - block if no free block
+}
+
+//===========================
+// [4] FREE BLOCK:
+//===========================
+void free_block(void *va)
+{
+	//==================================================================================
+	//DON'T CHANGE THESE LINES==========================================================
+	//==================================================================================
+	{
+		assert((uint32)va >= dynAllocStart && (uint32)va < dynAllocEnd);
+	}
+	//==================================================================================
+	//==================================================================================
+
+	//TODO: [PROJECT'25.GM#1] DYNAMIC ALLOCATOR - #4 free_block
+	//Your code is here
+	//Comment the following line
+//		panic("free_block() Not implemented yet");
+
+
+	uint32 address = (uint32)va;
+
+	struct PageInfoElement *page = to_page_info(address);
+
+	int idx = 0;
+	for (int i = 0; i < NUM_OF_BLOCK_SIZES; ++i) {
+		if ((1 << (i + 3)) == page->block_size) // (+3 for starting from 8)
+		{
+			idx = i;
+			break;
+		}
+	}
+
+	struct BlockElement *block = (struct BlockElement*)address;
+	page->num_of_free_blocks++;
+	LIST_INSERT_TAIL(&freeBlockLists[idx], block);
+
+	// Free The Page
+	if (page->num_of_free_blocks == PAGE_SIZE / page->block_size) {
+		uint32 current_va = to_page_va(page);
+
+		for (int i = 0; i < page->num_of_free_blocks; ++i) {
+			struct BlockElement *block = (struct BlockElement*)current_va;
+			LIST_REMOVE(&freeBlockLists[idx], block);
+
+			current_va += page->block_size;
+		}
+
+		page->num_of_free_blocks = 0;
+		page->block_size = 0;
+
+		return_page((void*)to_page_va(page));
+		LIST_INSERT_TAIL(&freePagesList, page);
+	}
+
+}
+
+//==================================================================================//
+//============================== BONUS FUNCTIONS ===================================//
+//==================================================================================//
+
+//===========================
+// [1] REALLOCATE BLOCK:
+//===========================
+void *realloc_block(void* va, uint32 new_size)
+{
+    //TODO: [PROJECT'25.BONUS#2] KERNEL REALLOC - realloc_block
+    //Your code is here
+    //Comment the following line
+    //    panic("realloc_block() Not implemented yet");
+
+    if (va == NULL) {
+        return alloc_block(new_size);
+    }
+
+    if (new_size == 0) {
+        free_block(va);
+        return NULL;
+    }
+
+    uint32 old_size = get_block_size(va);
+
+//    if(new_size > DYN_ALLOC_MAX_BLOCK_SIZE){
+//        void *ret = kmalloc(new_size);
+//        if(ret) {
+//            memcpy(ret, va, old_size);
+//            free_block(va);
+//            return ret;
+//        }
+//        else return va;
+//    }
+
+    uint32 *new_va = alloc_block(new_size);
+    // Didn't found Size >= new_size
+    if (new_va == NULL) {
+        return (void *)va;
+    }
+
+    // Found Size >= new_size
+    uint32 size_after = get_block_size(new_va);
+    // if new_size <= old_size && i took size bigger than old_size
+    if (new_size <= old_size && size_after >= old_size) {
+        free_block(new_va);
+        return (void *)va;
+    }
+
+    uint32 min_size = (old_size < new_size ? old_size : new_size);
+
+    memcpy(new_va, va, min_size);
+    free_block(va);
+    return (void *)new_va;
+}
---
+++ kheap.c
@@ -0,0 +1,395 @@
+#include "kheap.h"
+
+#include <inc/memlayout.h>
+#include <inc/string.h>
+#include <inc/dynamic_allocator.h>
+#include <kern/conc/sleeplock.h>
+#include <kern/proc/user_environment.h>
+#include <kern/mem/memory_manager.h>
+#include "kern/conc/kspinlock.h"
+
+
+//==================================================================================//
+//============================== GIVEN FUNCTIONS ===================================//
+//==================================================================================//
+
+
+// My Helper Methods
+bool is_free(uint32 x){
+	int perm  = pt_get_page_permissions(ptr_page_directory,x);
+	if(perm & PERM_PRESENT) return 0;
+	if(perm & PERM_GUARD) return 0;
+	return 1;
+}
+
+
+uint32 *PhysAddrToVirtAddr_kheap;
+int PhysAddrToVirtAddr_ready = 0;
+
+//==============================================
+// [1] INITIALIZE KERNEL HEAP:
+//==============================================
+//TODO: [PROJECT'25.GM#2] KERNEL HEAP - #0 kheap_init [GIVEN]
+//Remember to initialize locks (if any)
+void kheap_init()
+{
+	//==================================================================================
+	//DON'T CHANGE THESE LINES==========================================================
+	//==================================================================================
+	{
+		initialize_dynamic_allocator(KERNEL_HEAP_START, KERNEL_HEAP_START + DYN_ALLOC_MAX_SIZE);
+		set_kheap_strategy(KHP_PLACE_CUSTOMFIT);
+		kheapPageAllocStart = dynAllocEnd + PAGE_SIZE;
+		kheapPageAllocBreak = kheapPageAllocStart;
+	}
+	//==================================================================================
+	//==================================================================================
+	init_kspinlock(&kheap_lk, "kheap");
+}
+
+//==============================================
+// [2] GET A PAGE FROM THE KERNEL FOR DA:
+//==============================================
+int get_page(void* va)
+{
+
+	int ret = alloc_page(ptr_page_directory, ROUNDDOWN((uint32)va, PAGE_SIZE), PERM_WRITEABLE, 1);
+	if (ret < 0)
+		panic("get_page() in kern: failed to allocate page from the kernel");
+	{
+		uint32 pa = virtual_to_physical(ptr_page_directory, ROUNDDOWN((uint32)va, PAGE_SIZE));
+		if (pa != 0 && PhysAddrToVirtAddr_kheap != NULL)
+			PhysAddrToVirtAddr_kheap[PPN(pa)] = ROUNDDOWN((uint32)va, PAGE_SIZE);
+	}
+
+	return 0;
+}
+
+//==============================================
+// [3] RETURN A PAGE FROM THE DA TO KERNEL:
+//==============================================
+void return_page(void* va)
+{
+	uint32 pa = virtual_to_physical(ptr_page_directory,ROUNDDOWN((uint32)va, PAGE_SIZE));
+	if (pa != 0 && PhysAddrToVirtAddr_kheap != NULL)
+		PhysAddrToVirtAddr_kheap[PPN(pa)] = 0;
+	unmap_frame(ptr_page_directory, ROUNDDOWN((uint32)va, PAGE_SIZE));
+}
+
+//==================================================================================//
+//============================ REQUIRED FUNCTIONS ==================================//
+//==================================================================================//
+//===================================
+// [1] ALLOCATE SPACE IN KERNEL HEAP:
+//===================================
+void* kmalloc(unsigned int size)
+{
+	//lock
+	acquire_kspinlock(&kheap_lk);
+
+	//TODO: [PROJECT'25.GM#2] KERNEL HEAP - #1 kmalloc
+	//Your code is here
+	//Comment the following line
+	//kpanic_into_prompt("kmalloc() is not implemented yet...!!");
+	if(size <=DYN_ALLOC_MAX_BLOCK_SIZE){
+		release_kspinlock(&kheap_lk);
+		return alloc_block(size);
+	}
+	int num_pages = (size+PAGE_SIZE-1)/PAGE_SIZE , page_size = num_pages * PAGE_SIZE;
+
+	uint32 start = 0,end = 0;
+	uint32 exact_s = 0,exact_e=0,worst_s=0,worst_e=0;
+	for(uint32 i = kheapPageAllocStart; i <kheapPageAllocBreak;i+=PAGE_SIZE){
+		if(is_free(i)){
+			end = i+PAGE_SIZE;
+			if(start == 0) start = i;
+		}
+		else {
+			if(end - start >=page_size){
+				//exact fit
+				if(end - start == page_size) {
+					exact_s= start,exact_e=end;
+					break;
+				}
+				// worst
+				else if( (end-start) > worst_e-worst_s){
+					worst_e = end,worst_s = start;
+				}
+			}
+			start= 0,end = 0;
+		}
+	}
+
+	uint32 page_s = 0,page_e = 0;
+	// exact fit
+	if(exact_s!=0) page_s = exact_s,page_e = exact_e;
+	// worst fit
+	else if(worst_s !=0) page_s = worst_s,page_e=worst_s + page_size;
+
+	// !exact and !worst
+	if(page_s == 0){
+//		cprintf("ana: %u \n",kheapPageAllocBreak);
+		if(kheapPageAllocBreak <= KERNEL_HEAP_MAX - page_size){
+
+			page_s = kheapPageAllocBreak;
+			page_e = kheapPageAllocBreak  + page_size;
+//			cprintf("ps: %u - pe: %u  - size: %u \n",page_s,page_e,page_size);
+			kheapPageAllocBreak = page_e;
+//			cprintf("ana2: %u \n",kheapPageAllocBreak);
+		}
+		else {
+			release_kspinlock(&kheap_lk);
+			return NULL;
+		}
+
+	}
+	for(uint32 i = page_s; i <page_e;i+=PAGE_SIZE){
+		uint32 idx = (i  - kheapPageAllocStart)/PAGE_SIZE;
+		allocs[idx].va = (void*)page_s;
+		allocs[idx].size = page_size;
+		get_page((void*)i);
+	}
+//	cprintf("k2: %u \n",kheapPageAllocBreak);
+	release_kspinlock(&kheap_lk);
+	return (void*)page_s;
+
+	//TODO: [PROJECT'25.BONUS#3] FAST PAGE ALLOCATOR
+
+}
+
+//=================================
+// [2] FREE SPACE FROM KERNEL HEAP:
+//=================================
+void kfree(void* virtual_address)
+{
+	acquire_kspinlock(&kheap_lk);
+	//TODO: [PROJECT'25.GM#2] KERNEL HEAP - #2 kfree
+	//Your code is here
+	//Comment the following line
+	//panic("kfree() is not implemented yet...!!");
+	//cprintf("%u\n", (KERNEL_HEAP_MAX - kheapPageAllocStart) / PAGE_SIZE); // 32766
+	if( (uint32)virtual_address < kheapPageAllocStart){
+		release_kspinlock(&kheap_lk);
+		return free_block(virtual_address);
+	}
+
+	uint32 idx = (((uint32)virtual_address) - kheapPageAllocStart)/PAGE_SIZE;
+	if(allocs[idx].va == NULL){
+		panic("kfree() in kern: Invalid Address\n");
+	}
+	uint32 page_start = (uint32)allocs[idx].va;
+
+	uint32 sz = allocs[idx].size;
+
+	uint32 page_end = page_start + sz;
+
+	for(uint32 i = page_start;i<page_end;i+=PAGE_SIZE){
+		uint32 idx = (i  - kheapPageAllocStart)/PAGE_SIZE;
+		allocs[idx].va = NULL;
+		allocs[idx].size = 0;
+		return_page((void*)i);
+	}
+
+	if(kheapPageAllocBreak == page_end){
+		kheapPageAllocBreak-=sz;
+		while(kheapPageAllocBreak > kheapPageAllocStart && is_free(kheapPageAllocBreak-PAGE_SIZE))kheapPageAllocBreak-=PAGE_SIZE;
+	}
+	release_kspinlock(&kheap_lk);
+
+}
+
+//=================================
+// [3] FIND VA OF GIVEN PA:
+//=================================
+unsigned int kheap_virtual_address(unsigned int physical_address)
+{
+	//TODO: [PROJECT'25.GM#2] KERNEL HEAP - #3 kheap_virtual_address
+	//Your code is here
+	//Comment the following line
+	//panic("kheap_virtual_address() is not implemented yet...!!");
+    numOfKheapVACalls++;
+    uint32 va = physical_to_virtual(ptr_page_directory, physical_address);
+    return (va == 0xFFFFFFFF) ? 0 : va;
+		/*EFFICIENT IMPLEMENTATION ~O(1) IS REQUIRED */
+}
+
+//=================================
+// [4] FIND PA OF GIVEN VA:
+//=================================
+unsigned int kheap_physical_address(unsigned int virtual_address)
+{
+	//TODO: [PROJECT'25.GM#2] KERNEL HEAP - #4 kheap_physical_address
+	//Your code is here
+	//Comment the following line
+	//panic("kheap_physical_address() is not implemented yet...!!");
+    return virtual_to_physical(ptr_page_directory, virtual_address);
+
+		/*EFFICIENT IMPLEMENTATION ~O(1) IS REQUIRED */
+}
+
+//=================================================================================//
+//============================== BONUS FUNCTION ===================================//
+//=================================================================================//
+// krealloc():
+
+//	Attempts to resize the allocated space at "virtual_address" to "new_size" bytes,
+//	possibly moving it in the heap.
+//	If successful, returns the new virtual_address, in which case the old virtual_address must no longer be accessed.
+//	On failure, returns a null pointer, and the old virtual_address remains valid.
+
+//	A call with virtual_address = null is equivalent to kmalloc().
+//	A call with new_size = zero is equivalent to kfree().
+
+extern __inline__ uint32 get_block_size(void *va);
+
+void *krealloc(void *virtual_address, uint32 new_size)
+{
+
+
+	//TODO: [PROJECT'25.BONUS#2] KERNEL REALLOC - krealloc
+	//Your code is here
+	//Comment the following line
+	//panic("krealloc() is not implemented yet...!!");
+
+
+	// ALLOCATE NEW SPACE IN KERNEL
+	if(virtual_address == NULL) {
+		if(new_size <=DYN_ALLOC_MAX_BLOCK_SIZE)return alloc_block(new_size);
+		return kmalloc(new_size);
+	}
+
+	uint32 va = (uint32)virtual_address;
+
+	// FREE SPACE FROM KERNEL
+	if(new_size == 0){
+		if(va < kheapPageAllocStart) free_block(virtual_address);
+		else kfree(virtual_address);
+		return NULL;
+	}
+
+	if(va >= dynAllocStart && va <=dynAllocEnd){
+		uint32 old_sz = get_block_size(virtual_address);
+		if(new_size <= DYN_ALLOC_MAX_BLOCK_SIZE) return realloc_block(virtual_address, new_size);
+		void *ret = kmalloc(new_size);
+		if(ret != NULL){
+			uint32 sz = old_sz < new_size ? old_sz : new_size;
+			memcpy(ret, virtual_address, sz);
+			free_block(virtual_address);
+		}
+		return ret;
+	}
+	// IF THE NEW SIZE NO MORE NEEDS PAGES
+	uint32 idx = (va  - kheapPageAllocStart)/PAGE_SIZE;
+
+	if(allocs[idx].va == NULL) panic("krealloc: invalid addr");
+
+	if(new_size <= DYN_ALLOC_MAX_BLOCK_SIZE){
+		uint32 old_size = allocs[idx].size;
+		if(old_size <=DYN_ALLOC_MAX_BLOCK_SIZE) return realloc_block(virtual_address,new_size);
+		void *ret = alloc_block(new_size);
+		if(ret != NULL){
+			uint32 sz = old_size < new_size ? old_size : new_size;
+			memcpy(ret, virtual_address, sz);
+			kfree(virtual_address);
+		}
+		return ret;
+	}
+
+	acquire_kspinlock(&kheap_lk);
+
+
+	va = (uint32)allocs[idx].va;
+
+	uint32 old_pages_size = allocs[idx].size;
+	uint32 num_pages = (new_size+PAGE_SIZE-1)/PAGE_SIZE , new_pages_size = num_pages * PAGE_SIZE;
+
+	if(old_pages_size == new_pages_size ){
+		release_kspinlock(&kheap_lk);
+		return (void*)va;
+	}
+
+
+	// IF THE NEW SIZE < OLD SIZE
+	if(new_pages_size < old_pages_size){
+
+		uint32 start = va + new_pages_size;
+		uint32 end = va + old_pages_size;
+		for(uint32 i = start ; i<end ; i+=PAGE_SIZE){
+			uint32 id = (i  - kheapPageAllocStart)/PAGE_SIZE;
+			allocs[id].va = NULL;
+			allocs[id].size = 0;
+			return_page((void*)i);
+		}
+		for(uint32 i = va ;i < va + new_pages_size;i+=PAGE_SIZE)
+			allocs[(i-kheapPageAllocStart)/PAGE_SIZE].size = new_pages_size;
+		release_kspinlock(&kheap_lk);
+		return (void*)va;
+
+	}
+
+	// Extend UP
+	bool can_extend =1;
+	uint32 n_start = va + old_pages_size;
+	uint32 n_end = va + new_pages_size;
+	if(n_end > KERNEL_HEAP_MAX)can_extend = 0;
+	else{
+		for(uint32 i = n_start;i<n_end;i+=PAGE_SIZE)
+			if(!is_free(i)){
+				can_extend = 0;
+				break;
+			}
+	}
+	if(can_extend){
+		for(uint32 i = va;i<n_end;i+=PAGE_SIZE){
+			uint32 idx= (i  - kheapPageAllocStart)/PAGE_SIZE;
+			allocs[idx].va = (void*)va;
+			allocs[idx].size = new_pages_size;
+			if(is_free(i))
+				get_page((void*)i);
+		}
+		if(n_end > kheapPageAllocBreak)kheapPageAllocBreak = n_end;
+		release_kspinlock(&kheap_lk);
+		return (void*) va;
+	}
+
+	//EXTEND DOWN
+	can_extend = 1;
+	uint32 end = va + old_pages_size;
+	n_start = end - new_pages_size;
+
+	if(new_pages_size > end || n_start < kheapPageAllocStart )
+	    can_extend = 0;
+
+	for(uint32 i = n_start; i < va; i += PAGE_SIZE){
+        uint32 idx = (i - kheapPageAllocStart)/PAGE_SIZE;
+	    if(!is_free(i) && allocs[idx].va != (void*)va){
+	        can_extend = 0;
+	        break;
+	    }
+	}
+
+	if(can_extend){
+	    for(uint32 i = n_start; i < end; i += PAGE_SIZE){
+	        uint32 idx = (i - kheapPageAllocStart)/PAGE_SIZE;
+	        allocs[idx].va = (void*)n_start;
+	        allocs[idx].size = new_pages_size;
+	        if(is_free(i))
+	            get_page((void*)i);
+	    }
+	    memmove((void*)n_start, (void*)va, old_pages_size);
+	    release_kspinlock(&kheap_lk);
+	    return (void*)n_start;
+	}
+
+	// CAN`T EXTEND
+
+	release_kspinlock(&kheap_lk);
+	void* n_va = kmalloc(new_size);
+	if(n_va != NULL){
+		uint32 sz = old_pages_size < new_size ? old_pages_size : new_size;
+		memcpy(n_va,virtual_address,sz);
+		kfree(virtual_address);
+	}
+	return n_va;
+
+}
